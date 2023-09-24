c44c4d05e9873ce2ef832c4744eb5a18427d2e9f
Pedro Paiva <pedro.paiva@bolt.eu>
2023-09-24 19:55:43 +0300

    fix scala/python submission response

--- core/dbt/adapters/base/impl.py
+++ core/dbt/adapters/base/impl.py
@@ -1292,9 +1292,9 @@ class BaseAdapter(metaclass=AdapterMeta):
         # process submission result to generate adapter response
         return self.generate_python_submission_response(submission_result)
 
-    def generate_scala_submission_response(self, submission_result: Any) -> AdapterResponse:
+    def generate_python_submission_response(self, submission_result: Any) -> AdapterResponse:
         raise NotImplementedError(
-            "Your adapter need to implement generate_scala_submission_response"
+            "Your adapter need to implement generate_python_submission_response"
         )
 
     @property
@@ -1322,6 +1322,11 @@ class BaseAdapter(metaclass=AdapterMeta):
         submission_result = job_helper.submit(compiled_code)
         # process submission result to generate adapter response
         return self.generate_scala_submission_response(submission_result)
+
+    def generate_scala_submission_response(self, submission_result: Any) -> AdapterResponse:
+        raise NotImplementedError(
+            "Your adapter need to implement generate_scala_submission_response"
+        )
 
     def valid_incremental_strategies(self):
         """The set of standard builtin strategies which this adapter supports out-of-the-box.
